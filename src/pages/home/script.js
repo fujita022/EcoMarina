02f05468f4b25be5e513d85c13300523ab9da3e9
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-06-06 19:55:33 -0300

    Mudanças JS, HTML, CSS
    Mudanças no html semantico:
     DIV -> UL & DIV -> LI
    
    Botões da nav com função de scrolling

---
+++ src/pages/home/script.js
@@ -0,0 +1,18 @@
+function scrolltosec1() {
+    document.getElementById('sec1').scrollIntoView({ behavior: 'smooth'});
+}
+function scrolltosec2() {
+    document.getElementById('sec2').scrollIntoView({ behavior: 'smooth'});
+}
+function scrolltosec3() {
+    document.getElementById('sec3').scrollIntoView({ behavior: 'smooth'});
+}
+function scrolltosec4() {
+    document.getElementById('sec4').scrollIntoView({ behavior: 'smooth'});
+}
+function scrolltosec5() {
+    document.getElementById('sec5').scrollIntoView({ behavior: 'smooth'});
+}
+function scrolltosec6() {
+    document.getElementById('sec6').scrollIntoView({ behavior: 'smooth'});
+}
